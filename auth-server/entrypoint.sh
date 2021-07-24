ffb1418d729f86697a37f9cd1594233a5b5d7b19
cat <liyi_hfuu@163.com>
2021-07-24 13:54:01 +0800

    :art: spring cloud kubernetes & remove eureka

--- auth-server/entrypoint.sh
+++ auth-server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ENV="-Denv=docker"
 
-export JAVA_OPTS="-server -Xmx256m -Xms256m -XX:+UseG1GC"
+export JAVA_OPTS="-server -Xmx256m -Xms256m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:/opt/logs/gc.log"
 export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8 -Djava.security.egd=file:/dev/./urandom"
 
 exec java ${ENV} $JAVA_OPTS -jar /home/auth-server/auth-server.jar
